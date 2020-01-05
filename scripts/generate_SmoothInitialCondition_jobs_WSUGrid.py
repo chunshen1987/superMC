bca41c1facb7867ab4962dce53dce149278cd040
Chun Shen <chunshen1987@gmail.com>
2020-01-05 15:13:07 -0500

    add new script to automatically run jobs to generate smoothed averaged initial conditions

--- scripts/generate_SmoothInitialCondition_jobs_WSUGrid.py
+++ scripts/generate_SmoothInitialCondition_jobs_WSUGrid.py
@@ -6,7 +6,11 @@ import subprocess
 import shutil
 from glob import glob
 
-def generate_script(folder_name, ecm):
+centrality_list = ['0-5', '5-10', '10-20', '20-30', '30-40', '40-50', '50-60',
+                   '60-70', '70-80', '80-90', '0-6', '6-15', '15-25', '25-35',
+                   '35-45', '45-55', '45-50', '0-10', '10-40', '40-80']
+
+def generate_script(folder_name, ecm, centrality_label):
     working_folder = path.join(path.abspath('./'), folder_name)
     walltime = '10:00:00'
 
@@ -26,9 +30,10 @@ def generate_script(folder_name, ecm):
 cd {2:s}
 rm -fr data
 mkdir data
-./superMC.e ecm={3:g} operation=9
+python3 generateAvgprofile.py -ecm {3:g} -cen {4:s} -collision_system Au+Au
 
-""".format(working_folder.split('/')[-1], walltime, working_folder, ecm))
+""".format(working_folder.split('/')[-1], walltime, working_folder, ecm,
+           centrality_label))
     script.close()
 
 
@@ -49,15 +54,21 @@ def generate_event_folder(working_folder, event_id, ecm):
     shutil.copyfile(
         path.join(code_path, 'parameters.dat'),
         path.join(working_folder, 'parameters.dat'))
-    generate_script(working_folder, ecm)
+    shutil.copyfile(
+        path.join(code_path, 'scripts/generateAvgprofile.py'),
+        path.join(working_folder, 'generateAvgprofile.py'))
+    subprocess.call("ln -s {0:s} {1:s}".format(
+        path.join(code_path, "scripts/centrality_cut_tables"),
+        path.join(working_folder, "centrality_cut_tables")), shell=True)
+    generate_script(working_folder, ecm, centrality_list[event_id])
 
 if __name__ == "__main__":
     try:
         folder_name = str(sys.argv[1])
-        ncore = int(sys.argv[2])
-        ecm = float(sys.argv[3])
+        ecm = float(sys.argv[2])
+        ncore = len(centrality_list)
     except IndexError:
-        print("./generate_jobs_guillimin.py working_folder num_of_cores")
+        print("{} working_folder ecm".format(str(sys.argv[0])))
         exit(0)
 
     for icore in range(ncore):
